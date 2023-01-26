0fa17071f8f1257d2f926dbfe8485fb44223f78b
David Rushing Dewhurst <david@pop-os.localdomain>
2023-01-26 13:22:56 -0500

    hotfix to structure

---
+++ ccyan/docs/annotated_dup.js
@@ -0,0 +1,7 @@
+var annotated_dup =
+[
+    [ "Categorical", "structCategorical.html", "structCategorical" ],
+    [ "Gamma", "structGamma.html", "structGamma" ],
+    [ "Normal", "structNormal.html", "structNormal" ],
+    [ "rngstate", "structrngstate.html", "structrngstate" ]
+];
---
+++ ccyan/docs/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "<cglnrs",
+  1: "cgnr",
+  2: "c",
+  3: "lrs",
+  4: "<"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Pages"
+};
+
